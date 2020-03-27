e47970a0ef99d8a91955c86da753d0a4ab21cf11
dejhlk0s <Laura.Kleuker@jungheinrich.de>
2020-03-27 12:54:46 +0100

    2ndChanges

--- dashboard/output_NSatz.php
+++ dashboard/output_NSatz.php
@@ -15,13 +15,9 @@
     <tbody>
             <?php foreach ($nSatzData as $nSatzDataset) { //Ausgabe der Suchergbnisse?>
     	        <tr id="<?php echo htmlspecialchars($dataset['masterId']) ?>"< onclick="selectRow(<?php echo htmlspecialchars($dataset['masterId'])?>)">
-		          <td style="width:auto"><?php echo htmlspecialchars($dataset['fzgLabel'])?></td>
-		          <td style="width:auto"><?php echo htmlspecialchars($dataset['fzgSop'])?></td>
-				  <td style="width:auto"><?php echo htmlspecialchars($dataset['brLabel'])?></td>
-				  <td style="width:auto"><?php echo htmlspecialchars($dataset['baKapa'])?></td>
-				  <td style="width:auto"><?php echo htmlspecialchars($dataset['baTyp'])?></td>
-				  <td style="width:auto"><?php echo htmlspecialchars($dataset['baMaterial'])?></td>
-				  <td style="width:auto"><?php echo htmlspecialchars($dataset['asLabel'])?></td>
+		          <td style="width:auto"><?php echo htmlspecialchars($nSatzDataset['fzgLabel'])?></td>
+		          <td style="width:auto"><?php echo htmlspecialchars($nSatzDataset['fzgSop'])?></td>
+				  <td style="width:auto"><?php echo htmlspecialchars($nSatzDataset['brLabel'])?></td>
 				</tr>
 	        <?php } ?>
     </tbody>
