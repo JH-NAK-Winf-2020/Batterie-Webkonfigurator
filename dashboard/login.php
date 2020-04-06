1a6d43600e8d48d7564e1b93a113a5c458b05070
dejhlk0s <Laura.Kleuker@jungheinrich.de>
2020-04-06 10:02:30 +0200

    materialnummern fixed

---
+++ dashboard/login.php
@@ -0,0 +1,13 @@
+<?php 
+session.start();
+?>
+
+<html>
+<h1>Login Seite</h1>
+<form action="" method="POST">
+   <input type="text" name="user" placeholder="Benutzername"><br>
+   <input type="password" name="password" placeholder="Passwort"><br>
+   <button type="submit" name="submit">Login</button>
+</form>
+
+</html>
