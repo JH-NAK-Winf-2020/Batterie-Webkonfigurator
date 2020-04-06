74b2c112e418307dc5beca99332c4237178889c1
DEJHAA0Q <Ahmad.asfour@jungheinrich.de>
2020-04-06 15:38:46 +0200

    Kommentare und Code Verbesserungen

--- dashboard/Insert.php
+++ dashboard/Insert.php
@@ -21,7 +21,7 @@ $resultSet = $conn->query("SELECT table_name FROM information_schema.tables
 <form method ="POST">
 <select class="select1" name="final_lit">
 
-<?php //Dropdown list of tables names
+<?php //Dropdown list for tables names
 echo "<option selected disabled>-- select table --</option>";
 while($rows = $resultSet->fetch_assoc())
 {
@@ -37,7 +37,7 @@ while($rows = $resultSet->fetch_assoc())
 <div class="SelectTable">
 <?php
 session_start();
-//"Select Table" button
+//'Select Table' button
 if(isset($_POST["submitTable"]))
 {   
     if (empty($_POST["final_lit"])) echo "Please select a table!";
@@ -50,14 +50,13 @@ if(isset($_POST["submitTable"]))
     $columnsResult = $conn->query("SELECT column_name FROM information_schema.columns 
                     WHERE table_schema = 'final_lit'
                     AND table_name = '$getTable'") or die($conn->error);
-//Get columns names. Create label and input fields
+//Get columns names and Create label and input fields
     $arrColumns = array();
     while($columns = $columnsResult->fetch_assoc())
     {
         $column_name=$columns['column_name'];
         if ($column_name == "id"){
             echo "<label for=$column_name> $column_name </label> <br> <input type = 'text' name = $column_name disabled> <br><br>";
-            //array_push($arrColumns, $column_name);
         } else {
             echo "<label for=$column_name> $column_name </label> <br> <input type = 'text' name = $column_name> <br><br>";
             array_push($arrColumns, $column_name);
@@ -70,17 +69,17 @@ if(isset($_POST["submitTable"]))
     $_SESSION['Insert2'] = $getTable;
     }   
 }
-//"Insert" button
+//'Insert' button
 if(isset($_POST["submitInsert"]))
 {
 
-//get Array values and table name from upper code
+//get Array values and table name from upper code.
     $arrColumns1 = $_SESSION['Insert1'];
     $_SESSION['Insert1'] = $arrColumns1;
     $getTable1 = $_SESSION['Insert2'];
     $_SESSION['Insert2'] = $getTable1;  
 
-//insert new values into array
+//add new values into array
     $newValues = array();
     foreach ($arrColumns1 as $value) 
     {
@@ -96,27 +95,19 @@ if(isset($_POST["submitInsert"]))
     $sepVal = implode("', '", $newValues);
     $sepVal = "'".$sepVal."'";
 
-//Insert new values into Database
+//Add new values into Database
     $sql = "INSERT INTO $getTable1 ($sepCol) VALUES ($sepVal)";
     if (mysqli_query($conn, $sql)) {
         echo "New values added to database 'final_lit' into table '".
-
                 $getTable1. "' successfully:". "<br><br>". $sepVal; 
-
-                $getTable1. "' successfully:". "<br><br>". $sepVal;
-
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 }
 echo "</form>";
 ?>
-
-
-  </div>
-
+</div>
 </center>
-
 </body>
  <footer>
       <h5>Diese Seite wurde von Studenten erstellt</h5>
